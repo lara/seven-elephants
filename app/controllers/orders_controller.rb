06b4eb0a32c70746c901387350e5797d912334b7
Lara Aydin <ayd.dilara@gmail.com>
2017-09-18 11:54:43 -0400

    Add ordering to cart items

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,5 @@
 class OrdersController < ApplicationController
   def current
-    @order_products_with_product = @order.order_products.includes(:product)
+    @order_products_with_product = @order.order_products.includes(:product).order(created_at: :desc)
   end
 end
