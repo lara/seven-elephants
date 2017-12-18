fbfe3bcfdf91638c2e48fa139964af089c2c1cda
Lara Aydin <ayd.dilara@gmail.com>
2017-12-17 21:04:29 -0500

    Fix indentation

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,30 +1,29 @@
 class ChargesController < ApplicationController
   def new; end
 
-def create
-  # Amount in cents
-  @amount = 500
+  def create
+    customer = Stripe::Customer.create(
+      :email => params[:stripeEmail],
+      :source  => params[:stripeToken]
+    )
 
-  customer = Stripe::Customer.create(
-    :email => params[:stripeEmail],
-    :source  => params[:stripeToken]
-  )
+    charge = Stripe::Charge.create(
+      :customer    => customer.id,
+      :amount      => @order.total,
+      :currency    => "usd"
+    )
 
-  charge = Stripe::Charge.create(
-    :customer    => customer.id,
-    :amount      => @amount,
-    :description => 'Rails Stripe customer',
-    :currency    => 'usd'
-  )
     @order.order_products.each do |order_product|
       order_product.product.stock_quantity -= order_product.quantity
       order_product.product.save
     end
 
-rescue Stripe::CardError => e
-  flash[:error] = e.message
-  redirect_to new_charge_path
-end
     @order.update(placed_at: Time.now)
     expire_shopping_cart
+
+  rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to new_charge_path
+
+  end
 end
