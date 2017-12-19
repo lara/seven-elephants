deb494fe9ff4fea273816984f558ed4f3331abd6
Lara Aydin <ayd.dilara@gmail.com>
2017-12-19 15:10:43 -0500

    Add minimagick to resize uploaded images

--- app/uploaders/product_images_uploader.rb
+++ app/uploaders/product_images_uploader.rb
@@ -1,14 +1,10 @@
 class ProductImagesUploader < CarrierWave::Uploader::Base
-  # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
-  # Choose what kind of storage to use for this uploader:
-  # storage :file
   storage :fog
 
-  # Override the directory where uploaded files will be stored.
-  # This is a sensible default for uploaders that are meant to be mounted:
+  process resize_to_fill: [1000, 1000]
+
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
