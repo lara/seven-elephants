e123ecd2d2021cbb4e476da3a69807afa77052fe
Lara Aydin <ayd.dilara@gmail.com>
2017-12-30 16:41:56 -0500

    Redirect to product page once order product is clicked from order --no need to display order product

--- app/controllers/admin/order_products_controller.rb
+++ app/controllers/admin/order_products_controller.rb
@@ -9,13 +9,19 @@ module Admin
     #     page(params[:page]).
     #     per(10)
     # end
+    before_action :redirect_to_product
 
     # Define a custom finder by overriding the `find_resource` method:
     # def find_resource(param)
     #   OrderProduct.find_by!(slug: param)
     # end
+    private
 
     # See https://administrate-prototype.herokuapp.com/customizing_controller_actions
     # for more information
+    def redirect_to_product
+      order_product = OrderProduct.find(params[:id])
+      redirect_to admin_product_path(order_product.product)
+    end
   end
 end
