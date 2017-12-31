2406627459e6f76d52dabab023c800150988bbe2
Lara Aydin <ayd.dilara@gmail.com>
2017-12-30 19:16:23 -0500

    Add order shipment notification email

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -5,4 +5,9 @@ class OrderMailer < ApplicationMailer
     @order = order
     mail(to: @order.email, subject: "Your Seven Elephants Order Confirmation")
   end
+
+  def order_shipped_email(order)
+    @order = order
+    mail(to: @order.email, subject: "Your Seven Elephants Order Shipment Notification")
+  end
 end
