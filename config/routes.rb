3008d9c9568765c9bd90f075ed166d7111c86cbc
Lara Aydin <ayd.dilara@gmail.com>
2018-01-11 14:11:22 -0500

    Add shipping steps

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,12 @@ Rails.application.routes.draw do
   root "products#index"
 
   get :cart, to: "orders#current"
+  get :shipping_method, to: "orders#shipping_rates_index"
+  post :update_shipping_method, to: "orders#update_shipping_method"
   get :checkout, to: "orders#checkout"
+  get :shipping_info, to: "shipping_infos#new", as: :new_shipping_info
+  post :shipping_info, to: "shipping_infos#create", as: :shipping_info
+
   resources :order_products, only: %i[create update destroy]
   resources :products, only: %i[index show]
   resources :charges
