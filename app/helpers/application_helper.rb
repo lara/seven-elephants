9ebef379b8f5ca31da923a514aaf0cf3f91f0582
Lara Aydin <ayd.dilara@gmail.com>
2018-01-16 13:20:48 -0500

    Add weight to product show page with the help of a helper function to convert between grams and pounds

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def format_price(price_in_cents)
     number_to_currency(price_in_cents * 0.01)
   end
+
+  def grams_to_pounds(grams)
+    (grams * 0.00220462).round(2)
+  end
+
+  def pounds_to_grams(pounds)
+    (pounds * 453.592).round(2)
+  end
 end
