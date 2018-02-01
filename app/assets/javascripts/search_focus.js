bc3cacc373c056f7c601a74e3ffccf64339ebab0
Lara Aydin <ayd.dilara@gmail.com>
2018-02-01 11:22:40 -0500

    Make search bar work in ios

---
+++ app/assets/javascripts/search_focus.js
@@ -0,0 +1,6 @@
+// const searchLabel = document.querySelector('.search-bar label')
+// const searchField = document.querySelector('.search-bar input')
+//
+// searchLabel.addEventListener('click', () => {
+//   searchField.focus()
+// })
