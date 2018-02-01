67e4e94524e8b80228d664998588daaa1ca9038c
Lara Aydin <ayd.dilara@gmail.com>
2018-02-01 11:31:39 -0500

    Skip yarn

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,7 +21,6 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-
   config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
 
   # Compress JavaScripts and CSS.
